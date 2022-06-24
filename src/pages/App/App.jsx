08620b79efd42831dc30b4b680eb0071500df52d
heysungj <linjing0525@gmail.com>
2022-06-24 09:33:51 -0400

    added some pages

--- src/pages/App/App.jsx
+++ src/pages/App/App.jsx
@@ -1,5 +1,11 @@
 import "./App.css";
+import { useState } from "react";
+import AuthPage from "../AuthPage/AuthPage";
+import NewOrderPage from "../NewOrderPage/NewOrderPage";
+import OrderHistoryPage from "../OrderHistoryPage/OrderHistoryPage";
 
 export default function App() {
-  return <main className="App">App</main>;
+  const [user, setUser] = useState(null);
+
+  return <main className="App">{user ? <NewOrderPage /> : <AuthPage />}</main>;
 }
